9bc45daaf05073cab799a235df60c7525509ded4
MH Shuvo <mhshuvoalways@gmail.com>
2024-08-02 14:42:02 +0600

    refactor: refactor for solving issue

--- frontend/app/components/settings/currency/Exchange.tsx
+++ frontend/app/components/settings/currency/Exchange.tsx
@@ -2,12 +2,11 @@
 
 import Button from "@/app/components/common/button/GradientButton";
 import ListBox from "@/app/components/common/headlessui/ListBox";
-import Input from "@/app/components/common/input/Input";
 import Currencies from "@/app/data/currencies.json";
+import { useState } from "react";
 
-const lists = ["Front (৳ 1000)", "End (1000 ৳)"];
+const lists = ["Front ($ 1000)", "End (1000 $)"];
 const decimalPoint = [
-  "none",
   "1.0",
   "1.00",
   "1.000",
@@ -19,27 +18,60 @@ const decimalPoint = [
 interface Props {}
 
 const Exchange: React.FC<Props> = () => {
+  const [currency, setCurrency] = useState({
+    unitPosition: lists[0],
+    deciamPoint: decimalPoint[0],
+    changeInto: Currencies[0],
+  });
+
+  const unitPositionHandler = (value: string) => {
+    setCurrency({
+      ...currency,
+      unitPosition: value,
+    });
+  };
+
+  const decimalPointHandler = (value: string) => {
+    setCurrency({
+      ...currency,
+      deciamPoint: value,
+    });
+  };
+
+  const changeIntoHandler = (value: string) => {
+    setCurrency({
+      ...currency,
+      changeInto: value,
+    });
+  };
+
   return (
     <div className="card">
       <p className="text2">Currency Setting</p>
       <div className="mt-5 space-y-3">
         <div className="space-y-2">
-          <label className="font-medium">Unit Positions</label>
-          <ListBox list={lists} />
+          <label className="font-medium">Unit Position</label>
+          <ListBox
+            value={currency.unitPosition}
+            items={lists}
+            onChangeHandler={unitPositionHandler}
+          />
         </div>
         <div className="space-y-2">
           <label className="font-medium">Decimal Point</label>
-          <ListBox list={decimalPoint} />
+          <ListBox
+            value={currency.deciamPoint}
+            items={decimalPoint}
+            onChangeHandler={decimalPointHandler}
+          />
         </div>
-        <div className="grid grid-cols-2 gap-3">
-          <div className="space-y-2">
-            <label className="font-medium">Current Currency</label>
-            <Input value="USD" />
-          </div>
-          <div className="space-y-2">
-            <label className="font-medium">Change into</label>
-            <ListBox list={Currencies} />
-          </div>
+        <div className="space-y-2">
+          <label className="font-medium">Change Into</label>
+          <ListBox
+            value={currency.changeInto}
+            items={Currencies}
+            onChangeHandler={changeIntoHandler}
+          />
         </div>
       </div>
       <Button name="Exchange" className="mt-5 w-full" />
